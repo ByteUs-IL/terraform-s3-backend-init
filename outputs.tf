453d3c73a1eb309027ec166de8c9f405f58b86e0
Philip Levin <72705461+DeOriginal@users.noreply.github.com>
2024-04-28 22:24:03 +0300

    changes the outputs to represent the values required by backends

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,19 @@
-output "aws_kms_key_id" {
+output "bucket" {
+  description = "The name of the S3 bucket holding the state file"
+  value       = one(module.s3_backend[*].name)
+}
+
+output "dynamodb_table" {
+  description = "The name of DynamoDB table holding the state lock"
+  value       = aws_dynamodb_table.backend.name
+}
+
+output "region" {
+  description = "The region where the state file is located at"
+  value       = var.aws_region
+}
+
+output "kms_key_id" {
   description = "The id of the Customer-Managed KMS key encrypting the s3 backend"
-  value       = one(module.s3_backend[*].aws_kms_key_id)
-}
-
-output "aws_s3_bucket_id" {
-  description = "The id of the S3 bucket holding the state file"
-  value       = one(module.s3_backend[*].aws_s3_bucket_id)
-}
-
-output "aws_dynamodb_table_arn" {
-  description = "The arn of DynamoDB table holding the state lock"
-  value       = aws_dynamodb_table.backend.arn
+  value       = one(module.s3_backend[*].kms_key_id)
 }
