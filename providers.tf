70491d80a78117f1d78847ad46e9efd14f91bea8
Philip Levin <72705461+DeOriginal@users.noreply.github.com>
2024-04-28 22:16:51 +0300

    updated aws provider version

--- providers.tf
+++ providers.tf
@@ -2,12 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.47.0"
     }
   }
 }
 
-# Configure the AWS Provider on Ireland
 provider "aws" {
   region = var.aws_region
 
